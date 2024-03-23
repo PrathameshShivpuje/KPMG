d9294c93c74b184c21f56002cd98cd45df8205af
PrathameshShivpuje <139623766+PrathameshShivpuje@users.noreply.github.com>
2024-03-23 17:26:17 +0530

    Updated metadata.sh

--- Challenge-2/metadata.sh
+++ Challenge-2/metadata.sh
@@ -1,1 +1,24 @@
-"}"  
+#!/bin/bash
+
+metadata=$(curl -sH Metadata:true "http://169.254.169.254/metadata/instance?api-version=2019-08-15")
+
+location=$(echo $metadata | jq -r '.compute.location')
+resource_group=$(echo $metadata | jq -r '.compute.resourceGroupName')
+vm_name=$(echo $metadata | jq -r '.compute.name')
+vm_size=$(echo $metadata | jq -r '.compute.vmSize')
+subscription_id=$(echo $metadata | jq -r '.compute.subscriptionId')
+os_type=$(echo $metadata | jq -r '.compute.osType')
+os_version=$(echo $metadata | jq -r '.compute.osVersion')
+
+echo "{"
+echo '  "provider": "Azure",'
+echo '  "metadata": {'
+echo '    "location": "'"$location"'",'
+echo '    "resource_group": "'"$resource_group"'",'
+echo '    "vm_name": "'"$vm_name"'",'
+echo '    "vm_size": "'"$vm_size"'",'
+echo '    "subscription_id": "'"$subscription_id"'",'
+echo '    "os_type": "'"$os_type"'",'
+echo '    "os_version": "'"$os_version"'"'
+echo "  }"
+echo "}"
